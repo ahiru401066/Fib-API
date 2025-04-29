b8dc3e22d5b6003efc72936c134efe32bd1e6d4e
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-04-29 19:05:25 +0900

    エラー処理の記述

--- backend/internal/handler/hello.go
+++ backend/internal/handler/hello.go
@@ -1,10 +1,26 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type ErrorResponse struct {
+	Status int `json:status`
+	Message string `json:message`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ErrorResponse{
+			Status: http.StatusBadRequest,
+			Message: "BadRequest",
+		})
+		return 
+	}
+
 	fmt.Fprintf(w, "hello GO!")
 }
