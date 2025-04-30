7b29525593868e819ceb88f610282ed2c8cab203
Ichikawa75 <101092289+ahiru401066@users.noreply.github.com>
2025-04-30 11:35:17 +0900

    テストコードの修正

--- backend/internal/service/fib_test.go
+++ backend/internal/service/fib_test.go
@@ -1,21 +1,24 @@
 package service
 
-import "testing"
+import (
+	"testing"
+	"math/big"
+)
 
 func TestFib(t *testing.T) {
 	testcase := []struct {
 		name           string
 		input          int
-		expectedOutput int64
+		expectedOutput *big.Int
 		expectedError  bool
 	}{
-		{"n=10", 10, 55, false},
-		{"n=2", 2, 1, false},
-		{"n=1", 1, 1, false},
-		{"n=0", 0, 0, true},
-		{"n=-1", -1, 0, true},
-		{"n=-2", -2, 0, true},
-		{"n=-5", -5, 0, true},
+		{"n=10", 10, big.NewInt(55), false},
+		{"n=2", 2, big.NewInt(1), false},
+		{"n=1", 1, big.NewInt(1), false},
+		{"n=0", 0, big.NewInt(0), true},
+		{"n=-1", -1, big.NewInt(0), true},
+		{"n=-2", -2, big.NewInt(0), true},
+		{"n=-5", -5, big.NewInt(0), true},
 	}
 
 	for _, tc := range testcase {
@@ -33,7 +36,7 @@ func TestFib(t *testing.T) {
 				return
 			}
 
-			if result != tc.expectedOutput {
+			if result.Cmp(tc.expectedOutput) != 0 {
 				t.Error("計算結果が一致しない")
 			}
 		})
